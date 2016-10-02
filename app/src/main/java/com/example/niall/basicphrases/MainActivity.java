b105986303cf9380e61a2946488a743c5cf29c47
Niall <niall.moore1993@gmail.com>
2016-10-02 16:08:20 +0100

    Added sound files. Began setup of button to sound clip mapping

--- app/src/main/java/com/example/niall/basicphrases/MainActivity.java
+++ app/src/main/java/com/example/niall/basicphrases/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.niall.basicphrases;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+    private HashMap<Integer, Integer> buttonIdToSoundClipId = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupButtonMapping();
+    }
+
+    private void setupButtonMapping() {
+        buttonIdToSoundClipId.put(R.id.button0, R.raw.hello);
+        buttonIdToSoundClipId.put(R.id.button1, R.raw.howareyou);
+    }
+
+    public void playPhrase(View view) {
+        if (mediaPlayer != null && mediaPlayer.isPlaying()) {
+            mediaPlayer.stop();
+        }
+        mediaPlayer = MediaPlayer.create(this, buttonIdToSoundClipId.get(view.getId()));
+        mediaPlayer.start();
     }
 }
